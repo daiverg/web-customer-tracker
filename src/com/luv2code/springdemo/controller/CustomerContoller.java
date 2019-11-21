eb769c0d54c2528cc9cc025f924f0f852fd54674
Dmitrij <seo.wolf.d@gmail.com>
2019-11-21 19:51:54 +0100

    Chapter 30 part3, Spring & Hibernate. Controller+Entity+Service+DAO

--- src/com/luv2code/springdemo/controller/CustomerContoller.java
+++ src/com/luv2code/springdemo/controller/CustomerContoller.java
@@ -1,15 +1,34 @@
 package com.luv2code.springdemo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.luv2code.springdemo.dao.CustomerDAO;
+import com.luv2code.springdemo.entity.Customer;
+import com.luv2code.springdemo.service.CustomerService;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerContoller {
 	
-	@RequestMapping("/list")
+// inject customer service
+	@Autowired
+	private CustomerService customerService;
+	
+	@GetMapping("/list")
 	public String listCustomers(Model theModel) {
+		
+//		get customer from the service
+		List<Customer> theCustomer = customerService.getCustomers();
+		
+//		add customers to the model
+		theModel.addAttribute("customers", theCustomer);
+
 		
 		return "list-customers";
 	}
---
+++ src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -0,0 +1,38 @@
+package com.luv2code.springdemo.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.luv2code.springdemo.entity.Customer;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO {
+
+//	inject the session factory
+	@Autowired
+	private SessionFactory sessionFactory;
+			
+	@Override
+	public List<Customer> getCustomers() {
+//		current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+//		query
+		Query<Customer> theQuery = currentSession.createQuery("from Customer", Customer.class);
+		
+		
+//		execute query and get result
+		List<Customer> customers = theQuery.getResultList();
+		
+//		return result
+		
+		return customers;
+	}
+
+}
